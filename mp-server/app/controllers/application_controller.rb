eb8013df07919f492f65071b047f8cd9b94ba03d
the-alchemist-gh <malikbanks01@gmail.com>
2022-09-15 23:09:41 +0000

    Created get requests for db

--- mp-server/app/controllers/application_controller.rb
+++ mp-server/app/controllers/application_controller.rb
@@ -1,6 +1,24 @@
-class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
-  # Add routes
-  
-end
+
+  class ApplicationController < Sinatra::Base
+    set :default_content_type, 'application/json'
+    
+    # Add routes
+    get '/missings' do
+      # "Hello"
+      missing_people = Missing.all.order(:created_at)
+      missing_people.to_json(include: :last_seens)
+    end
+
+    get '/users' do
+      # "Hello"
+      user = User.all.order(:created_at)
+      user.to_json(include: [:missings , :last_seens ])
+    end
+
+    get '/last_seens' do
+      # "Hello"
+      last_seen = LastSeen.all.order(:created_at)
+      last_seen.to_json
+    end
+
+  end
--- mp-server/db/seeds.rb
+++ mp-server/db/seeds.rb
@@ -1,26 +1,157 @@
 puts "🌱 Seeding messages..."
 
-Message.create([
+User.create([
   {
-    body: "Hello 👋",
-    username: "Liza"
+    name: "Liza Kotey",
+    email: "lizak@gmail.com",
+    password: "lizak"
   },
   {
-    body: "Hi!",
-    username: "Duane"
+    name: "Bernard Avle",
+    email: "bavle@gmail.com",
+    password: "bavle"
   },
   {
-    body: "let's get this chat app working",
-    username: "Liza"
+    name: "Malik Jabel",
+    email: "malik@gmail.com",
+    password: "malik"
   },
   {
-    body: "ngl, this looks like a lot 😬",
-    username: "Duane"
-  },
-  {
-    body: "You got this! 💪",
-    username: "Liza"
+    name: "Ibrahim Abdul",
+    email: "ibrahim@gmail.com",
+    password: "ibrahim"
   }
 ])
+
+Missing.create(
+  [
+    {
+      name: "Kila Ofori",
+      age: 23,
+      description: "Short hair with dark skin",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzS6FhNHXYLwYxxU1EkVzjKZO8HBRcqFQJhUjk50qzTYmLe0vwLv_uzH2IkLKZgnifB-M&usqp=CAU",
+      location: "Adenta",
+      date_missing: 12/10/2018,
+      found: false,
+      user_id: 3
+    },
+    {
+      name: "Akua Donkor",
+      age: 49,
+      description: "Long hair with blue skin",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF4bZQ0XueSie3UgP8coJ_3mDBge5TT1vcIe2CFTdq33OM5GrHkQqhceYjvWHfhGM6ibs&usqp=CAU",
+      location: "Adenta",
+      date_missing: 12/01/2018,
+      found: false,
+      user_id: 2
+    },
+    {
+      name: "Kwame jokoto",
+      age: 12,
+      description: "Short hair with long legs",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzS6FhNHXYLwYxxU1EkVzjKZO8HBRcqFQJhUjk50qzTYmLe0vwLv_uzH2IkLKZgnifB-M&usqp=CAU",
+      location: "Achimota",
+      date_missing: 12/10/2018,
+      found: false,
+      user_id: 3
+    },
+    {
+      name: "Kofi Baboni",
+      age: 1,
+      description: "Short hair with dark skin",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzS6FhNHXYLwYxxU1EkVzjKZO8HBRcqFQJhUjk50qzTYmLe0vwLv_uzH2IkLKZgnifB-M&usqp=CAU",
+      location: "Adenta",
+      date_missing: 03/02/2022,
+      found: true,
+      user_id: 2
+    },
+    {
+      name: "Aku Sika",
+      age: 50,
+      description: "Short hair with dark skin",
+      image: "https://pickaface.net/gallery/avatar/20121012_072718_1430_RandomPerson.png",
+      location: "Tema",
+      date_missing: 01/10/2021,
+      found: false,
+      user_id: 3
+    }
+  ]
+)
+
+
+LastSeen.create(
+  [
+    {
+      location_seen: "Madina",
+      comments: "He was wearing a blue shirt",
+      missing_id: 2,
+      user_id: 3,
+      date_seen: 02/03/2020
+    },
+    {
+      location_seen: "Accra",
+      comments: "He was wearing a white shirt",
+      missing_id: 2,
+      user_id: 4,
+      date_seen: 02/04/2020
+    },
+    {
+      location_seen: "Tulaku",
+      comments: "Couldn't talk to him",
+      missing_id: 3,
+      user_id: 1,
+      date_seen: 02/03/2020
+    },
+    {
+      location_seen: "Somanya",
+      comments: "He was wearing no shirt",
+      missing_id: 3,
+      user_id: 2,
+      date_seen: 02/03/2020
+    },
+    {
+      location_seen: "Madina",
+      comments: "He was wearing a blue shirt",
+      missing_id: 4,
+      user_id: 3,
+      date_seen: 02/03/2020
+    },
+    {
+      location_seen: "Kumasi",
+      comments: "He was wearing a blue shirt",
+      missing_id: 4,
+      user_id: 4,
+      date_seen: 02/03/2020
+    },
+    {
+      location_seen: "Tamale",
+      comments: "He was wearing a blue shirt",
+      missing_id: 5,
+      user_id: 1,
+      date_seen: 02/03/2020
+    },
+    {
+      location_seen: "Atomic",
+      comments: "He was wearing a blue shirt",
+      missing_id: 5,
+      user_id: 2,
+      date_seen: 02/03/2020
+    },
+    {
+      location_seen: "Redco",
+      comments: "He was wearing a blue shirt",
+      missing_id: 1,
+      user_id: 3,
+      date_seen: 02/03/2020
+    },
+    {
+      location_seen: "Koftown",
+      comments: "He was wearing a blue shirt",
+      missing_id: 1,
+      user_id: 4,
+      date_seen: 02/03/2020
+    }
+  ]
+)
 
 puts "✅ Done seeding!"
