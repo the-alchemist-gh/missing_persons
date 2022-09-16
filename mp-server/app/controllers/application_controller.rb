81e4c66ee7172e47ad5a9b06682e390e29ee3ab3
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-09-16 18:46:24 +0000

    Update the backend to had server post requests

--- mp-server/app/controllers/application_controller.rb
+++ mp-server/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 
   class ApplicationController < Sinatra::Base
     set :default_content_type, 'application/json'
-    
+
     # Add routes
     get '/missings' do
       # "Hello"
@@ -19,6 +19,20 @@
       # "Hello"
       last_seen = LastSeen.all.order(:created_at)
       last_seen.to_json
+    end
+
+    post '/missings' do
+      missing_people = Missing.create(
+        name: params[:name],
+        age: params[:age],
+        description: params[:description],
+        image: params[:image],
+        location: params[:location],
+        date_missing: params[:date_missing],
+        found: params[:found],
+        user_id: params[:user_id]
+      )
+      missing_people.to_json
     end
 
   end
