18874a29e49b3a3e415a2eb4a67e3167238d3d7c
Nii <bnstephens001@gmail.com>
2022-09-17 10:46:35 +0000

    Added the patch to application Controller in mp-server

--- mp-server/app/controllers/application_controller.rb
+++ mp-server/app/controllers/application_controller.rb
@@ -34,5 +34,42 @@
       )
       missing_people.to_json
     end
+    patch '/missings/:id' do
+      missing_people = Missing.find(params[:id])
+      missing_people.update(
+        name: params[:name],
+        age: params[:age],
+        description: params[:description],
+        image: params[:image],
+        location: params[:location],
+        date_missing: params[:date_missing],
+        found: params[:found],
+        user_id: params[:user_id]
+      )
+      missing_people.to_json
+    end
+
+    patch '/users/:id' do
+      users= User.find(params[:id])
+      users.update(
+       name: params[:name],
+        email: params[:email],
+        password: params[:password],
+      )
+      users.to_json
+    end
+    
+    patch '/last_seens/:id' do
+      reviews= LastSeen.find(params[:id])
+      reviews.update(
+       location_seen: params[:location_seen],
+        comments: params[:comments],
+        missing_id: params[:missing_id],
+        user_id: params[:user_id]
+        date_seen: params[:date_seen]
+      )
+      reviews.to_json
+    end
+
 
   end
