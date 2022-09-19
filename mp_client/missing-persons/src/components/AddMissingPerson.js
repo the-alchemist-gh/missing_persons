c5fe8cde28c03c3d3f22bca8863e8e9874e2b0b6
Nii <bnstephens001@gmail.com>
2022-09-19 13:23:05 +0000

    Added codebase for User, Comments and Missing Person

--- mp_client/missing-persons/src/components/AddMissingPerson.js
+++ mp_client/missing-persons/src/components/AddMissingPerson.js
@@ -53,70 +53,70 @@ function NewPerson({getFormData, logInId}){
  
   return (
     <>
-          <div className="flex items-center justify-center content-center bg-gray-100">
-            <div className="px-8 py-6 mx-4 mt-4 text-left bg-white shadow-lg md:w-1/3 lg:w-1/3 sm:w-1/3">
-              <div className="flex justify-between text-teal-600">
-                <h3 className="text-2xl font-bold text-center text-blue-900">Report a missing person</h3>
-                <NavLink to="/" className="text-xs flex text-gray-600">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                  </svg>
-                  Cancel</NavLink>
-              </div>
-              
-              <form onSubmit={handleSubmit}>
-                <div className="mt-4">
-                  <div>
-                    <label className="block" for="Name">Full Name</label>
-                    <input type="text" required placeholder="John Doe" name="name" onChange={handleChange}  value={personFormData.name} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1"/>
-                  </div>
-                  <div className="mt-4">
-                    <label className="block" for="Name">How old is this person</label>
-                    <input type="number" required placeholder="Age of the missing person" name="age" min="1" onChange={handleChange}  value={personFormData.age} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1"/>
-                  </div>
-                  <div className="mt-4">
-                    <label className="block" for="Name">When did you last see this person?</label>
-                    <input type="date" required placeholder="Date person went missen" name="date_missing" onChange={handleChange}  value={personFormData.date_missing} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1"/>
-                  </div>
-                  <div className="mt-4">
-                    <label for="locations" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Where did you last see this person</label>
-                    <select id="categories" required defaultValue="Select location" name="location" onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                      <option value="">Select location</option>
-                      <option value="Adenta">Adenta</option>
-                      <option value="Madina">Madina</option>
-                      <option value="Lapaz">Lapaz</option>
-                      <option value="Achimota">Achimota</option>
-                      <option value="Accra">Accra</option>
-                      <option value="Accra">Tema</option>
-                      <option value="Accra">Kumasi</option>
-                      <option value="Accra">Cape Coast</option>
-                    </select>
-                  </div>  
-                  <div className="mt-4">
-                    <label for="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Describe the missing person</label>
-                    <textarea id="description" rows="3" name="description" onChange={handleChange}  value={personFormData.description} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" placeholder="Eg, Very skinny, was wearing a black long dress"></textarea>
-                  </div>
-                  <div className="mt-4 flex justify-between">
-                    <div className="w-3/5">
-                      <label className="block" for="Image">Person's Image</label>
-                      <input type="text" name="image" onChange={handleChange}  value={personFormData.image} placeholder="Kindly enter image url" className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1"/>
-                      <span className="text-xs text-gray-400">Enter the full url of the image.</span>
-                    </div>
-                    <div className="w-2/5 ml-2 relative rounded-md border-dashed h-24 border-2">
-                      {
-                        personFormData.image !== "" ? <img className="h-full w-full rounded-md object-cover" src={personFormData.image} alt={personFormData.name} /> : null
-                      }
-                      
-                    </div>
-                    
-                  </div>
-                  <div className="flex">
-                    <button type="submit" className="w-full px-6 py-2 mt-4 text-white bg-black rounded-lg hover:bg-gray-500">Add Missing Person Now</button>
-                  </div>
-                </div>
-              </form>
-            </div>
+      <div className="flex items-center justify-center content-center bg-gray-100">
+        <div className="px-8 py-6 mx-4 mt-4 text-left bg-white shadow-lg md:w-1/3 lg:w-1/3 sm:w-1/3">
+          <div className="flex justify-between text-teal-600">
+            <h3 className="text-2xl font-bold text-center text-blue-900">Report a missing person</h3>
+            <NavLink to="/" className="text-xs flex text-gray-600">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+              </svg>
+              Cancel</NavLink>
           </div>
+          
+          <form onSubmit={handleSubmit}>
+            <div className="mt-4">
+              <div>
+                <label className="block" for="Name">Full Name</label>
+                <input type="text" required placeholder="John Doe" name="name" onChange={handleChange}  value={personFormData.name} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1"/>
+              </div>
+              <div className="mt-4">
+                <label className="block" for="Name">How old is this person</label>
+                <input type="number" required placeholder="Age of the missing person" name="age" min="1" onChange={handleChange}  value={personFormData.age} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1"/>
+              </div>
+              <div className="mt-4">
+                <label className="block" for="Name">When did you last see this person?</label>
+                <input type="date" required placeholder="Date person went missen" name="date_missing" onChange={handleChange}  value={personFormData.date_missing} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1"/>
+              </div>
+              <div className="mt-4">
+                <label for="locations" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Where did you last see this person</label>
+                <select id="categories" required defaultValue="Select location" name="location" onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                  <option value="">Select location</option>
+                  <option value="Adenta">Adenta</option>
+                  <option value="Madina">Madina</option>
+                  <option value="Lapaz">Lapaz</option>
+                  <option value="Achimota">Achimota</option>
+                  <option value="Accra">Accra</option>
+                  <option value="Accra">Tema</option>
+                  <option value="Accra">Kumasi</option>
+                  <option value="Accra">Cape Coast</option>
+                </select>
+              </div>  
+              <div className="mt-4">
+                <label for="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Describe the missing person</label>
+                <textarea id="description" rows="3" name="description" onChange={handleChange}  value={personFormData.description} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" placeholder="Eg, Very skinny, was wearing a black long dress"></textarea>
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div className="w-3/5">
+                  <label className="block" for="Image">Person's Image</label>
+                  <input type="text" name="image" onChange={handleChange}  value={personFormData.image} placeholder="Kindly enter image url" className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1"/>
+                  <span className="text-xs text-gray-400">Enter the full url of the image.</span>
+                </div>
+                <div className="w-2/5 ml-2 relative rounded-md border-dashed h-24 border-2">
+                  {
+                    personFormData.image !== "" ? <img className="h-full w-full rounded-md object-cover" src={personFormData.image} alt={personFormData.name} /> : null
+                  }
+                  
+                </div>
+                
+              </div>
+              <div className="flex">
+                <button type="submit" className="w-full px-6 py-2 mt-4 text-white bg-black rounded-lg hover:bg-gray-500">Add Missing Person Now</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   ) 
 }
--- mp_client/missing-persons/src/components/Register.js
+++ mp_client/missing-persons/src/components/Register.js
@@ -45,53 +45,53 @@ function Register(){
 
   return (
     <>
-          <div className="flex items-center justify-center content-center bg-gray-100">
-            <div className="px-8 py-6 mx-4 mt-4 text-left bg-white shadow-lg md:w-1/3 lg:w-1/3 sm:w-1/3">
-                  <div className="mt-6 text-1xl bg-red-100 rounded-md border-2 border-red-600 font-bold text-red-600 text-center">
-                    This is just a test project, please do not use your correct email or password to register.
-                  </div>
-              <h3 className="text-2xl font-bold text-center text-blue-900">Register for an account now</h3>
-              <form onSubmit={handleSubmit}>
-                <div className="mt-4">
-                  <div>
-                    <label className="block" for="Name">Name</label>
-                    <input type="text" placeholder="Name" name="name" onChange={handleChange}  value={registerDataState.name} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
-                  </div>
-                  <div className="mt-4">
-                    <label className="block" for="email">Email</label>
-                    <input type="text" placeholder="Email" name="email" onChange={handleChange}  value={registerDataState.email} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
-                  </div>
-                  <div className="mt-4">
-                    <label className="block">Password</label>
-                      <input type="password" placeholder="Password" name="password" onChange={handleChange}  value={registerDataState.password} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
-                  </div>
-                  <div className="mt-4">
-                    <label className="block">Confirm Password</label>
-                      <input type="password" placeholder="Password" onChange={handleChange} name="password2" value={registerDataState.password2} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
-                      
-                  </div>
-                  <div className="flex">
-                    <button type="submit" className="w-full px-6 py-2 mt-4 text-white bg-black rounded-lg hover:bg-gray-500">Get Started</button>
-                  </div>
-                  <div className="mt-6 text-grey-dark">
-                    Already have an account? <span> 
-                      <NavLink className="text-blue-900 hover:underline" to="/login">
-                        Log in
-                      </NavLink>
-                    </span>
-                    
-                  </div>
-                  <div className="mt-6 text-grey-dark">
-                    Go back to <span> 
-                      <NavLink className="text-blue-900 hover:underline" to="/">
-                        HomePage
-                      </NavLink>
-                    </span>
-                  </div>
-                </div>
-              </form>
+      <div className="flex items-center justify-center content-center bg-gray-100">
+        <div className="px-8 py-6 mx-4 mt-4 text-left bg-white shadow-lg md:w-1/3 lg:w-1/3 sm:w-1/3">
+              <div className="mt-6 text-1xl bg-red-100 rounded-md border-2 border-red-600 font-bold text-red-600 text-center">
+                This is just a test project, please do not use your correct email or password to register.
+              </div>
+          <h3 className="text-2xl font-bold text-center text-blue-900">Register for an account now</h3>
+          <form onSubmit={handleSubmit}>
+            <div className="mt-4">
+              <div>
+                <label className="block" for="Name">Name</label>
+                <input type="text" placeholder="Name" name="name" onChange={handleChange}  value={registerDataState.name} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
+              </div>
+              <div className="mt-4">
+                <label className="block" for="email">Email</label>
+                <input type="text" placeholder="Email" name="email" onChange={handleChange}  value={registerDataState.email} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
+              </div>
+              <div className="mt-4">
+                <label className="block">Password</label>
+                  <input type="password" placeholder="Password" name="password" onChange={handleChange}  value={registerDataState.password} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
+              </div>
+              <div className="mt-4">
+                <label className="block">Confirm Password</label>
+                  <input type="password" placeholder="Password" onChange={handleChange} name="password2" value={registerDataState.password2} className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
+                  
+              </div>
+              <div className="flex">
+                <button type="submit" className="w-full px-6 py-2 mt-4 text-white bg-black rounded-lg hover:bg-gray-500">Get Started</button>
+              </div>
+              <div className="mt-6 text-grey-dark">
+                Already have an account? <span> 
+                  <NavLink className="text-blue-900 hover:underline" to="/login">
+                    Log in
+                  </NavLink>
+                </span>
+                
+              </div>
+              <div className="mt-6 text-grey-dark">
+                Go back to <span> 
+                  <NavLink className="text-blue-900 hover:underline" to="/">
+                    HomePage
+                  </NavLink>
+                </span>
+              </div>
             </div>
-          </div>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
